a2738a5a0e134cd64814dac146ef7139504d6eea
Baraa Al-Masri <baraa.masri@asu.edu.jo>
2020-03-03 00:01:06 -0800

    lol

---
+++ stringeg.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class styringeg {
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        int a,b;
+        String name;
+        System.out.println("what's your name?");
+        name = read.next();
+        System.out.println("my name is : " + (name) );
+
+        System.out.println("How old R U ?");
+
+        b = read.nextInt();
+        System.out.println("I'm : " + (b) + " years old ");
+
+
+
+
+    }
+}
