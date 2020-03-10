ee17201159960a2a57ee77426a1b16705952cd3e
Baraa Al-Masri <baraa.masri@asu.edu.jo>
2020-03-10 01:01:08 -0700

    Update bloom.java

--- moo/bloom.java
+++ moo/bloom.java
@@ -1,4 +1,5 @@
 package moo;
+import java.lang.Math;
 
 public class bloom {
     public static void main(String[] args){
@@ -13,5 +14,8 @@ public class bloom {
 
         p1.print();
         p2.print();
+        //calculating the distance
+        double dist1 = Math.sqrt( Math.pow( ( p2.x - p1.x) , 2 ) + Math.pow( ( p2.y - p1.y) , 2 ) );
+        System.out.println("Distance between p1 & p2" + dist1);
     }
 }
