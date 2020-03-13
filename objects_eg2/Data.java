c70a8471580600513072a7d9a7e8b59dad53ec75
baraa-almasri <baraa.masri@asu.edu.jo>
2020-03-13 21:40:55 +0200

    created @ Fri 13 Mar 21:40:55 EET 2020

---
+++ objects_eg2/Data.java
@@ -0,0 +1,11 @@
+package objects_eg2;
+public class Data {
+    String name;
+    double gpa;
+    int ID;
+    void list(){
+        System.out.println("Student's name: " + name +
+                "\nStudent's GPA: " + gpa +
+                "\nStudent's ID: " + ID);
+    }
+}
