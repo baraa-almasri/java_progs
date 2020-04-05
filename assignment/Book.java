e70f7a98dd95fae7d0931657edfb7d938d1eb46a
Baraa Al-Masri <baraa.masri@asu.edu.jo>
2020-04-05 11:01:29 +0300

    created @ Sun  5 Apr 11:00:46 EEST 2020

---
+++ assignment/Book.java
@@ -0,0 +1,46 @@
+package assignment;
+
+public class Book {
+    private String bookName , ISBN , authorName , publisher;
+    private int pages;
+    //empty constructor
+    public Book(){
+        // \0 is NULL in ascii
+        this.bookName = "\0";
+        this.ISBN = "\0";
+        this.authorName = "\0";
+        this.publisher = "\0";
+        this.pages = 0;
+    }
+    public Book(String bookName , String ISBN ,String authorName ,String publisher,int pages ){
+        this.bookName = bookName;
+        this.ISBN = ISBN;
+        this.authorName = authorName;
+        this.publisher = publisher;
+        this.pages = pages;
+    }
+    void setBookName(String book){
+        this.bookName = book;
+    }
+    void setISBN(String isbn){
+        this.ISBN = isbn;
+    }
+    void setAuthorName(String auth){
+        this.authorName = auth;
+    }
+    void setPublisher(String pub){
+        this.publisher = pub;
+    }
+    void setPages(int pages){
+        this.pages = pages;
+    }
+    void getBookInfo(){
+        System.out.printf("Book title: %s\n",this.bookName);
+        System.out.printf("ISBN: %s\n",this.ISBN);
+        System.out.printf("Book's author: %s\n",this.authorName);
+        System.out.printf("Book's publisher: %s\n",this.publisher);
+        System.out.printf("Number of pages: %d\n",this.pages);
+        System.out.printf("\n");
+
+    }
+}
