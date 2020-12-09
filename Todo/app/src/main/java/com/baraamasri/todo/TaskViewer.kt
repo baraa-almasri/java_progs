4956e3fa868fd7aa6d15f0d6f6db71b68bb6c714
Baraa Al-Masri <hexagon16rpm@yandex.com>
2020-12-09 14:33:08 +0200

    hmm

--- Todo/app/src/main/java/com/baraamasri/todo/TaskViewer.kt
+++ Todo/app/src/main/java/com/baraamasri/todo/TaskViewer.kt
@@ -1,9 +1,11 @@
 package com.baraamasri.todo
 
+import android.content.DialogInterface
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_add_task.*
 import kotlinx.android.synthetic.main.activity_task_viewer.*
 
@@ -31,9 +33,30 @@ class TaskViewer : AppCompatActivity() {
     }
 
     fun deleteTask(view: View) {
+        showDeleteDialog()
+
+        //finish()
+    }
+
+    private fun showDeleteDialog() {
+        val dialogBuilder = AlertDialog.Builder(this)
+
+        dialogBuilder.setMessage("Are you sure?").setCancelable(false)
+            .setPositiveButton("Yes", DialogInterface.OnClickListener{
+                dialog, id ->
+                deleteFromDB()
+                finish()
+            }).setNegativeButton("NO", DialogInterface.OnClickListener {
+                    dialog, id -> dialog.cancel()
+            })
+
+        val alert = dialogBuilder.create()
+        alert.setTitle("Really?!")
+        alert.show()
+    }
+
+    private fun deleteFromDB() {
         this.db = this.dbHelper.writableDatabase
         this.db.execSQL("DELETE FROM `tasks` WHERE `id` = ?", arrayOf(this.taskID))
-
-        finish()
     }
 }
